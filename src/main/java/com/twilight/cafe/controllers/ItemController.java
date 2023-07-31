1e08ce30c60acd9af919ee307c1f34686139e6a8
manikandan <manikandan.ponnurangam@twilightsoftwares.com>
2023-07-31 12:10:34 +0530

    Remove Print

--- src/main/java/com/twilight/cafe/controllers/ItemController.java
+++ src/main/java/com/twilight/cafe/controllers/ItemController.java
@@ -31,15 +31,7 @@ public class ItemController {
 	public String getText()
 	{
 		int i=0;
-		System.out.println(i);
-		System.out.println(i);
-		System.out.println(i);
-		System.out.println(i);
-		System.out.println(i);
-		System.out.println(i);
-		System.out.println(i);
-		System.out.println(i);
-		System.out.println(i);
+		
 		return "Hello Wo";
 	}
 
